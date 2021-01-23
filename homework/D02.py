ba4c3884c0e98d5a262227520a37b7bb51c2a614
佩蓁 竺 <406570209@mail.fju.edu.tw>
2021-01-23 23:48:01 +0800

    20210123 11:47

--- homework/D02.py
+++ homework/D02.py
@@ -7,8 +7,13 @@ print(array1)
 
 #2.呈上題的array，找出被6除餘1的數的索引
 
+#答案： [0,5],[1,0],[2,1],[3,2],[4,3]
+print(np.where(array1 % 6 == 1))
+#也可寫成這樣：
+print(np.where(np.mod(array1,6) == 1))
 #如果元素值為被6除餘1的數的話就用 "Y" 來替代，反之則是 "N"
 print(np.where(array1 % 6 == 1,"Y","N"))
+
 
 
 
