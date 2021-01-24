4cbe24dfd867e59fa2829447638128cdd352267e
佩蓁 竺 <406570209@mail.fju.edu.tw>
2021-01-24 20:27:47 +0800

    20210124 D06訂正

--- homework/D06.py
+++ homework/D06.py
@@ -10,6 +10,12 @@ with open('multi_array.npz','wb') as f:
 a = np.load('multi_array.npz')
 print(a['a'])
 print(a['b'])
+
+#解答
+array1 = np.array(range(30))
+array2 = np.array([2,3,5])
+np.savez('array.npz',array1,array2)
+
 #2. 讀取剛剛的npz檔，加入下列array一起存成新的npz檔
 with open('new_multi_array.npz','wb') as f:
     np.savez(f,a=array1,b=array2,c=np.array(range(10)))
@@ -17,3 +23,8 @@ b = np.load('new_multi_array.npz')
 print(b['a'])
 print(b['b'])
 print(b['c'])
+
+#解答
+load_array = np.load('array.npz')
+array3 = np.array([[4,5,6],[1,2,3]])
+np.savez('new_array.npz',load_array['arr_0'],load_array['arr_1'],array3)
