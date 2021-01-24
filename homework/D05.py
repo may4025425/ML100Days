70e418b5fc2db76a63eaaf89683d5c4c9d25a78c
佩蓁 竺 <406570209@mail.fju.edu.tw>
2021-01-24 15:13:23 +0800

    20210124 D05修正

--- homework/D05.py
+++ homework/D05.py
@@ -30,10 +30,25 @@ print(np.max(math_score))
 print(np.min(math_score))
 #標準差
 print(np.std(math_score))
+
+#解答
+math_score[4] = 55
+print("補考數學分數後 ","平均",np.nanmean(math_score),"最大值",np.nanmax(math_score),"最小值",np.nanmin(math_score),"標準差",np.nanstd(math_score))
 #3. 用補考後資料找出與國文成績相關係數最高的學科?
+
+#correlate():是計算兩個一維序列的互相關，並非計算相關係數!
 en_ch_corr = np.correlate(english_score,chinese_score)
 ma_ch_corr = np.correlate(math_score,chinese_score)
 if(en_ch_corr > ma_ch_corr):
+    print("英文成績與國文成績的相關係數最高")
+else:
+    print("數學成績與國文成績的相關係數最高")
+#解答
+ch_ma_corr = np.corrcoef(chinese_score,math_score)
+ch_en_corr = np.corrcoef(chinese_score,english_score)
+print("國文與數學相關係數",ch_ma_corr)
+print("國文與英文相關係數",ch_en_corr)
+if(str(ch_en_corr > ma_ch_corr)):
     print("英文成績與國文成績的相關係數最高")
 else:
     print("數學成績與國文成績的相關係數最高")
