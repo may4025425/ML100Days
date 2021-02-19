aa073f5f4886d10c3061764dee63b0768e85e562
佩蓁 竺 <406570209@mail.fju.edu.tw>
2021-02-19 14:32:36 +0800

    20210219 D09

--- homework/D09.py
+++ homework/D09.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 #讀取資料夾中boston.csv讀取其欄位CHAS、NOX、RM，輸出成.xlsx檔案
 bosto_data = pd.read_csv(r'C:\Users\may40\Downloads\boston.csv',usecols=['CHAS','NOX','RM'])
-print(bosto_data.to_excel('my_boston.xlsx'))
-
+bosto_data = pd.to_excel('my_boston.xlsx')
+print(bosto_data)
---
+++ homework/D11.py
@@ -0,0 +1,8 @@
+import re
+
+import numpy as np
+
+print(np.nan)
+print(np.isnan(np.nan))
+print(np.NAN)
+print(np.isnan(np.NAN))
