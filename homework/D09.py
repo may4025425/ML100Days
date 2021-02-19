a97aa328dc2ace24bc537e0b6d767287006e16cc
佩蓁 竺 <406570209@mail.fju.edu.tw>
2021-02-19 14:56:37 +0800

    20210219 D09、D10修正

--- homework/D09.py
+++ homework/D09.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 #讀取資料夾中boston.csv讀取其欄位CHAS、NOX、RM，輸出成.xlsx檔案
-bosto_data = pd.read_csv(r'C:\Users\may40\PycharmProjects\DataSciencePractice\homework\boston.csv',usecols=['CHAS','NOX','RM'])
-bosto_data = pd.to_excel('my_boston.xlsx')
+bosto_data = pd.read_csv('boston.csv',usecols=['CHAS','NOX','RM'])
+bosto_data.to_excel('my_boston.xlsx')
 print(bosto_data)
--- homework/D10.py
+++ homework/D10.py
@@ -8,4 +8,4 @@ stock2 = pd.read_csv('STOCK_DAY_0050_202010.csv')
 stock_data = pd.concat([stock1,stock2])
 
 #找出open小於close的資料
-stock_data[stock_data.open<stock_data.close]
+print(stock_data[stock_data.open<stock_data.close])
