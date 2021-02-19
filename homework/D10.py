cb87ac06e0f113044be0db3a6eaf6873f088bafe
佩蓁 竺 <406570209@mail.fju.edu.tw>
2021-02-19 14:48:43 +0800

    20210219 D10

--- homework/D10.py
+++ homework/D10.py
@@ -1,7 +1,11 @@
-import os
+import pandas as pd
 
-print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+#讀取STOCK_DAY_0050_202009.csv、STOCK_DAY_0050_202010.csv
+stock1 = pd.read_csv('STOCK_DAY_0050_202009.csv')
+stock2 = pd.read_csv('STOCK_DAY_0050_202010.csv')
 
-print("PATH:", os.environ.get('PATH'))
+#串聯
+stock_data = pd.concat([stock1,stock2])
 
-
+#找出open小於close的資料
+stock_data[stock_data.open<stock_data.close]
